96a5ee5e40b555ddcef8a1a0c1fd0686dab3bb1a
Paul Harwood <paul@harwood-leon.com>
2016-08-24 20:43:52 +0100

    Changed parties to venues

--- client/main.js
+++ client/main.js
@@ -1,14 +1,14 @@
 // All Tomorrow's Parties -- client
 
 Meteor.subscribe("directory");
-Meteor.subscribe("parties");
+Meteor.subscribe("venues");
 
 Meteor.startup(function () {
   Meteor.autorun(function () {
     if (! Session.get("selected")) {
-      var party = Parties.findOne();
-      if (party) {
-        Session.set("selected", party._id);
+      var venue = Venues.findOne();
+      if (venue) {
+        Session.set("selected", venue._id);
       }
     }
   });
